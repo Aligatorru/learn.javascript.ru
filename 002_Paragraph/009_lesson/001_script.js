82602b9bc76ea1f32d267db64150ae9d2125969c
Evgeny <ya.aligatorru@gmail.com>
2023-03-26 12:05:46 +0300

    Часть 1, глава 2 Основы JavaScript

---
+++ 002_Paragraph/009_lesson/001_script.js
@@ -0,0 +1,24 @@
+// Операторы сравнения
+// важность: 5
+// Каким будет результат этих выражений?
+
+5 > 4                   
+alert( 5 > 4  );  //true
+
+"ананас" > "яблоко"
+alert( "ананас" > "яблоко" ); // false
+
+"2" > "12"
+alert( "2" > "12" ); // true
+
+undefined == null
+alert( undefined == null ); // true
+
+undefined === null
+alert( undefined === null ); //false
+
+null == "\n0\n"
+alert( null == "\n0\n" ); // false
+
+null === +"\n0\n"
+alert( null === +"\n0\n" ); //false
